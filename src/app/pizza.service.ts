daf93190f30b0853b95c8e7ed2683b73e7b0d41b
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-02-19 11:39:45 -0600

    changed imports, made an array, console.logged it
    new array in the logs

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //pretend we make a service call here
+
+    const getPizzaToppingsFromWebService = [
+      {
+        name: 'olives'
+        , price: 2.25
+      }
+      ,{
+        name: 'pepperoni'
+        , price: 1.50
+      }
+      , {
+        name: 'sausage'
+        , price: 1.75
+      }
+    ]
+
+    return getPizzaToppingsFromWebService; 
+  }
 }
