fb91bf7678dcc5a10d7a94c138b8ae8cf4aa61d2
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-02-28 10:37:43 -0600

    new component for slack n tell week 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NumberComponent } from './number/number.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    , PizzaToppingsComponent
+    , PizzaToppingsComponent, NumberComponent
   ],
   imports: [
     BrowserModule,
