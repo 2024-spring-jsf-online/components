6a855854d3c5588a6cd096f2120cfe7bfcab947f
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-02-29 13:13:04 -0600

    new component done

--- src/app/caj-number/caj-number.component.ts
+++ src/app/caj-number/caj-number.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-caj-number',
@@ -16,5 +16,7 @@ export class CajNumberComponent {
   color = "";
 
   favColor = () => this.color;
+
+  
 
 }
