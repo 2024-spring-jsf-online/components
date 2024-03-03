0b8acb263eaffc9e915c26b58e9d3e59798b1a5f
Peter Brummel <pbrummel@gmail.com>
2024-03-02 20:54:37 -0600

    updated pbrummel-addititon.component.ts
    Added in 3 different types of pizza, along with pricings

--- src/app/pbrummel-addition/pbrummel-addition.component.ts
+++ src/app/pbrummel-addition/pbrummel-addition.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pbrummel-addition',
   templateUrl: './pbrummel-addition.component.html',
   styleUrl: './pbrummel-addition.component.css'
 })
-export class PbrummelAdditionComponent {
+export class PbrummelAdditionComponent implements OnInit {
+  pizzaMenu = [
+    { name: 'Margherita', price: 9.99, description: 'Classic tomato and mozzarella' },
+    { name: 'Pepperoni', price: 12.99, description: 'Pepperoni, tomato, and mozzarella' },
+    { name: 'Vegetarian', price: 11.99, description: 'Assorted vegetables and mozzarella' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
