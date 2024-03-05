de0f126af18ab3d82724cb3c5fe4e683b7c61cfd
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-03-05 11:24:27 -0600

    Generate my addition component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatChipsModule } from '@angular/material/chips';
 
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
-import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
-import { JtaDropdownComponent } from './jta-dropdown/jta-dropdown.component';
-import { SnamAdditionComponent } from './snam-addition/snam-addition.component';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MtunaAdditionComponent } from './mtuna-addition/mtuna-addition.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaToppingsComponent,
-    TjsMultiplicationComponent,
-    JtaDropdownComponent,
-    SnamAdditionComponent
+    MtunaAdditionComponent
   ],
   imports: [
     BrowserModule,
@@ -35,12 +26,7 @@ import { SnamAdditionComponent } from './snam-addition/snam-addition.component';
     MatCardModule,
     MatButtonModule,
     MatCheckboxModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatSlideToggleModule,
-    MatChipsModule
+
   ],
   providers: [
     provideAnimationsAsync()
