840c48ebec52793b6764471736f98c6ad1d64f51
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-03-05 20:24:11 -0600

    Addintion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MtunaAdditionComponent } from './mtuna-addition/mtuna-addition.component';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -26,6 +27,8 @@ import { MtunaAdditionComponent } from './mtuna-addition/mtuna-addition.componen
     MatCardModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
 
   ],
   providers: [
