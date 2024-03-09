ae714cf9efd1ea282b900c75b616e9ea57ff4c01
CEBlank <96022511+CEBlank@users.noreply.github.com>
2024-03-09 13:33:17 -0600

    Added webservice placeholder
    toppings from web service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //pretend there is a web service call, we'll learn it 
+    const toppingsFromWebService = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+      , {
+        name: 'Pepperoni'
+        , price: 1.50
+      }
+      , {
+        name: 'Sausage'
+        , price: 1.75
+      }
+    ];
+
+    return toppingsFromWebService;
+  }
 }
