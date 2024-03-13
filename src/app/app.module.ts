284449b9eb586e9ee26bdfb79f95753805f8e99a
snam4231 <160560567+snam4231@users.noreply.github.com>
2024-03-13 17:02:50 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
 import { JtaDropdownComponent } from './jta-dropdown/jta-dropdown.component';
 
