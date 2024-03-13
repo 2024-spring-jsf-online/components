0da09036663de58c156dbfe230c229922cc61be2
snam4231 <160560567+snam4231@users.noreply.github.com>
2024-03-13 14:27:20 -0500

    solve merge conflict
    solve merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,21 +15,35 @@ import { MatChipsModule } from '@angular/material/chips';
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SnamAdditionComponent } from './snam-addition/snam-addition.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
+import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
+import { JtaDropdownComponent } from './jta-dropdown/jta-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-    , PizzaToppingsComponent, SnamAdditionComponent
+    AppComponent,
+    PizzaToppingsComponent,
+    TjsMultiplicationComponent,
+    JtaDropdownComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    , FormsModule
-    , MatCardModule
-    , MatButtonModule
-    , MatCheckboxModule
-    , MatSlideToggleModule
-    , MatChipsModule
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [
     provideAnimationsAsync()
