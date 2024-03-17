aba3ae3ed1621c7d89d4371b4302bf04a79abbd0
PopcornProblems <126116250+PopcornProblems@users.noreply.github.com>
2024-03-17 14:24:09 -0500

    added pizza toppings display interface
    and transformed the data to the proper shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +14,7 @@ import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.componen
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
