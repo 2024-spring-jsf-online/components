9ec9ae20713c656d4a39e5ac33b41a58eafd466d
PopcornProblems <126116250+PopcornProblems@users.noreply.github.com>
2024-03-17 14:09:57 -0500

    Were calling a DI'd Service
    Next lets show the toppings in the component

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppings() {
+    //not getting from cloud
+
+    const toppingFrom = [
+      {
+        name: 'black olives'
+        , price: 1.25
+      }
+      , {
+        name: 'Pepperoni'
+        , price: 1.75
+      }
+      , {
+        name: 'onions'
+        , price: .99
+      }
+    ];
+    return toppingFrom;
+  }
 }
