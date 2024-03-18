a7850b2a31a67f3552b8cd1a3712f07c8f715e69
r-ignatius <131979982+r-ignatius@users.noreply.github.com>
2024-03-17 21:37:24 -0500

    Calling DI'd service

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './pizza-toppings.component.css'
 })
 export class PizzaToppingsComponent {
+  //Magic DI -- Dependency injection
+  constructor(
+    //annotated constructor parameter to be a PizzaService
+    //Code completetion said "Probably trying to import PizzaService from typescript file (line 2)"
+    //gives access to function written in pizza.service.ts (?)
+    private pizzaSvc: PizzaService
+    ) { }
+
+  //angular method called when component is created and is ready to init
+  ngOnInit(): void{
+    //calls pizzaSvc that was injected (DI) in constructor (line 15)
+    //calls function within pizza service ts
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud;
+    console.log(pt);
+  }
 
 }
