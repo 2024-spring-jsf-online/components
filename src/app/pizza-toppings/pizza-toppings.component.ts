bc5f35e4c749178b05b4038393c669fca0f73ffd
LittleBitMakes <54948332+emunro000@users.noreply.github.com>
2024-03-20 15:14:29 -0500

    Calling a DI'd sevice
    Next show the pizza toppings in component

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
   styleUrl: './pizza-toppings.component.css'
 })
-export class PizzaToppingsComponent {
+export class PizzaToppingsComponent implements OnInit {
 
+    // Magic dependency injection
+    constructor(
+        private pizzaSvc: PizzaService
+    ) { }
+
+    ngOnInit(): void {
+        const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
+        console.log(pt);
+    }
 }
