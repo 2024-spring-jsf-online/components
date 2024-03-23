1d22ed8cdbd624cb15bec46913e1228d9d5e6334
r-ignatius <131979982+r-ignatius@users.noreply.github.com>
2024-03-23 10:59:09 -0500

    Created S&T component
    Created component for week07 slack and tell

---
+++ src/app/rei-addition/rei-addition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rei-addition',
+  templateUrl: './rei-addition.component.html',
+  styleUrl: './rei-addition.component.css'
+})
+export class ReiAdditionComponent {
+
+}
